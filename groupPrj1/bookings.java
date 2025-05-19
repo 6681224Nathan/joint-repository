c95455a857a7daddd12a16a4372573f79b960cd3
unknown <nathan.tan@student.mahidol.edu>
2025-05-19 22:52:59 +0700

    file from jet

---
+++ groupPrj1/bookings.java
@@ -0,0 +1,16 @@
+package groupPrj1;
+
+class items{
+    private String code;
+    private String name;
+    private int unit_price;
+
+    public items(){}
+    public items(String Code, String Name, int Unit_price)
+    {
+        code=Code;
+        name=Name;
+        unit_price=Unit_price;
+    }
+
+}

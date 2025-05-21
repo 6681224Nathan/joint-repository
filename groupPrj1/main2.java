7e42dc95e076cb4378ff056262d9a36701e1cadf
unknown <nathan.tan@student.mahidol.edu>
2025-05-22 01:47:31 +0700

    project 1, can't believe we made it this far

--- groupPrj1/main2.java
+++ groupPrj1/main2.java
@@ -37,6 +37,61 @@ class Items{
 
 }
 
+class Discount{
+    private ArrayList<Double> discountPrice;
+    private ArrayList<Double> discountPercent;
+
+    public Discount()
+    {
+        discountPrice = new ArrayList<>();
+        discountPercent = new ArrayList<>();
+    }
+
+    public void addDiscount(double price, double percent)
+    {
+        discountPrice.add(price);
+        discountPercent.add(percent);
+    }
+
+    public double applyDiscount(double price)
+    {
+        for(int i=0; i<discountPrice.size() ; i++)
+        {
+            if(price >= discountPrice.get(i))
+            {
+                price = price * ( 1 - discountPercent.get(i)/100 );
+            }
+        }
+
+        return price;
+    }
+
+}
+
+class Booking{
+    private String bookings;
+    private String customers;
+    private int days;
+    private int[] rooms;
+    private int persons;
+    private int[] meals;
+    private int price;
+
+    public Booking(String B, String C, int D, int[] R, int P, int[] M)
+    {
+        bookings = B;
+        customers = C;
+        days =  D;
+        rooms = R.clone(); //array
+        persons = P;
+        meals = M.clone(); //array
+        price = 0;
+    }
+    
+    //booking, customer, days, rooms/day(R1:R2:R3), persons, meals/person/day(M1:M2:M3)
+}
+
+
 class main2{
     public static void main(String[] args){
 
@@ -45,49 +100,134 @@ class main2{
         Scanner keyboardScanner = new Scanner(System.in);
         int i=0;
         String path = ""; //I'll change it to src/main/Java/Ex2_6580969/countries.txt
+        
+        
+        //////////////////check the existence of the file, and read from it
         Items[] itemsList = new Items[6];
-        
-        
-        //check the existence of the file
 
-        File itemsFile = new File(path + "items.txt");
-        Scanner itemsScanner = null;
+        Scanner itemsScanner = createFileScanner(path, "items.txt");
 
-        while(itemsScanner == null){
+
+        //////////////////////get the information from "items.txt" file to 6 objects
+
+        itemsScanner.nextLine(); //skip the header line
+        for(i=0; itemsScanner.hasNext(); i++)
+        {
+            String line   = itemsScanner.nextLine();        
+            String []cols = line.split(",");        
+            
+            String Code = cols[0].trim();
+            String Name = cols[1].trim();
+            int Unit_price = Integer.parseInt(cols[2].trim());
+
+            System.out.printf("%s %s %d\n", Code, Name, Unit_price);
+            itemsList[i] = new Items(Code, Name, Unit_price);
+        }
+
+        /////////////////////////opening discount flies
+        Scanner discountsScanner = createFileScanner(path, "discounts.txt");
+        Discount discounts = new Discount();
+
+        discountsScanner.nextLine(); //skip header line
+        for(i=0; discountsScanner.hasNextLine(); i++)
+        {
+            String line = discountsScanner.nextLine();
+            String []cols = line.split(",");
+            
+            if(cols.length < 2) continue;
+            
+            double discountPrice = Double.parseDouble(cols[0].trim());
+            double discountPercent = Double.parseDouble(cols[1].trim());
+            System.out.printf("Price : %10.2f Criteria : %-7.2f\n", discountPrice, discountPercent);
+            discounts.addDiscount(discountPrice, discountPercent);
+            
+        }
+        
+
+        /////////////////////////opening the bookings file
+        
+        Scanner bookingsScanner = createFileScanner(path, "bookings.txt");
+
+        ArrayList<Booking> bookings = new ArrayList<Booking>();
+
+        bookingsScanner.nextLine(); //skip header line
+        for(i=0; bookingsScanner.hasNextLine(); i++)
+        {
             try
             {
-                itemsScanner = new Scanner(itemsFile);
+                String line = bookingsScanner.nextLine();
+                String []cols = line.split(",");
+                
+                
+                String bookingName = cols[1].trim();
+                String customers = cols[2].trim();
+                int days =  Integer.parseInt(cols[3].trim());
+                int[] rooms = cols[4].split(":");
+                int persons = 4;
+                int[] meals = M.clone();
+                
+                if(cols.length < 6) continue;
+                
+                double discountPrice = Double.parseDouble(cols[0].trim());
+                double discountPercent = Double.parseDouble(cols[1].trim());
+                System.out.printf("Price : %10.2f Criteria : %-7.2f\n", discountPrice, discountPercent);
+                discounts.addDiscount(discountPrice, discountPercent);
+            }
+            catch(Exception e)
+            {
+                
+            }
+            
+        }
+            
+        
+        
+        
+        itemsScanner.close();
+        discountsScanner.close();
+        bookingsScanner.close();
+        keyboardScanner.close();
+    }
+
+    ////////////////end of main, following these will be functions for main, for repeated use
+
+    public static Scanner createFileScanner(String path, String fileName) 
+    {
+        String filePath = path + fileName;
+        File file = new File(filePath);
+        Scanner keyboardScanner = new Scanner(System.in);
+
+        Scanner fileScanner = null;
+
+        while(fileScanner == null){
+            try
+            {
+                fileScanner = new Scanner(file);
             }
             catch(Exception e)
             {
                 System.err.println(e);
                 System.out.println("Enter correct file name (with.txt) = ");
                 String ia = keyboardScanner.next();
-                itemsFile = new File(path + ia);
+                file = new File(path + ia);
 
             }
         }
 
-        ////////////////////
+        return fileScanner;
+    }
 
-        itemsScanner.nextLine(); //skip the header line
-        for(i=0; itemsScanner.hasNext(); i++)
+    public static int arrayParseInt(String[] stringNumber)
+    {
+        int[] intNumber = new int[stringNumber.length];
+        for(int i=0; i<stringNumber.length; i++)
         {
-
-            String line   = itemsScanner.nextLine();        
-            String []cols = line.split(",");        
-            
-            String Code = cols[0].trim();
-            String Name = cols[1].trim();
-            int Unit_price = Integer
-
-            
+            intNumber[i] = Integer.parseInt(stringNumber[i].trim());
         }
 
-        
-        
-        
-        itemsScanner.close();
-        keyboardScanner.close();
+        return intNumber;
     }
+
+
+    
 }

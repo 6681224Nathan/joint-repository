9c28fcb07479f9aa0ed344fb08aa97f9c92026a8
6681224Nathan <nathan.tan@student.mahidol.edu>
2025-06-04 14:39:46 +0700

    save

---
+++ losalamos/src/main/java/test/main2.java
@@ -0,0 +1,31 @@
+package test;
+
+import java.util.*;
+
+class InvalidInputException extends Exception
+{
+    public InvalidInputException(String msg)
+    {
+        super(msg);
+    }
+}
+
+class main2
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
+
+interface morning
+{
+    public void sayMorning();
+}
+
+abstract class evening
+{
+    private String country;
+
+    
+}

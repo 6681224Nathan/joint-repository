fab83de0c8ed6caccbf2c22dda6898d38091d932
6681224Nathan <nathan.tan@student.mahidol.edu>
2025-06-09 23:07:48 +0700

    cookie run DP

---
+++ python/cookieRunDP.py
@@ -0,0 +1,21 @@
+A = [0,0,2,5,3,1,10,5,6,2,0]
+
+def f(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return A[2]
+    
+    dp = [0] * (n + 1)
+    dp[0] = 0
+    dp[1] = 0
+    dp[2] = A[2]
+    
+    for i in range(3, n+1):
+        dp[i] = max(dp[i-1], dp[i-3] + A[i])
+    
+    return dp[n]
+
+print(f(6))
